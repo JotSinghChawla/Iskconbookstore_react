89eb2e3fdff37a47d3297906f2be5b2846a6b326
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-09 03:21:12 +0530

    Added React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React,{ Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/Main';
 import { HashRouter } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
@@ -12,5 +14,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
