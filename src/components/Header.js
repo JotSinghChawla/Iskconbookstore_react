942e25ec8c4c5b04d1007fa3741000b626e30697
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-10 20:48:27 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,49 @@
-import React from 'react'
-import { Navbar, Nav, NavItem,Button, Container  }  from 'react-bootstrap'
+import { React, useState } from 'react'
+import { MDBNavbar, MDBCollapse, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler } from 'mdbreact'
+import SocialIcons from './SocialIcons'
 
 const navbarStyle ={
   backgroundColor: '#fada5e',
   fontWeight: 'bold',
-  fontSize: '18px'
+  fontSize: '18px',
+  color: '#584E4A',
+  padding: '5px 10px',
+  boxShadow: '0px 4px 16px 0px rgb(119 119 119 / 50%)',
 }
 
 const Header = () => {
+
+const [navbarOpen, setNavbarOpen] = useState(false)
+
     return (
-        <Navbar style={navbarStyle}collapseOnSelect expand="lg" fixed='top' variant="light">
-            <Navbar.Brand href="#home"> 
-              <img src={process.env.PUBLIC_URL + '/assets/images/demo.png'} height='35' width='35' className='d-inline-block align-top'
-              alt='Iskcon' /> ISKCON 
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls='responsive-navbar' />
-            <Navbar.Collapse id='responsive-navbar'>
-              <Nav className='mr-auto'>
-                <Nav.Link href='#'>Home</Nav.Link>
-                <Nav.Link href='#'>Bookstore</Nav.Link>
-                <Nav.Link href='https://www.iskcon.org/about-us/what-is-iskcon.php'>About us</Nav.Link>
-                <Nav.Link href='https://centres.iskcon.org/'>Contact us</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <MDBNavbar style={navbarStyle} color='dark' expand="md" fixed='top'>
+          <div className='container-fluid'>
+            <MDBNavbarBrand href="#home"> 
+              <img src={process.env.PUBLIC_URL + '/assets/images/demo.png'} height='45' width='45' className='d-inline-block'
+              alt="ISKCON - The Hare Krishna Movement" /> <strong> ISKCON </strong> 
+            </MDBNavbarBrand>
+            <MDBNavbarToggler onClick={ () => setNavbarOpen(true) } />
+            <MDBCollapse id='navbarCollapse' isOpen={navbarOpen} navbar >
+              <MDBNavbarNav className='ml-auto mr-auto'>
+
+                <MDBNavItem active>
+                  <MDBNavLink to="#">Home</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="#!">Bookstore</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="https://www.iskcon.org/about-us/what-is-iskcon.php">About us</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="https://centres.iskcon.org/">Contact us</MDBNavLink>
+                </MDBNavItem>
+
+              </MDBNavbarNav>
+            </MDBCollapse>
+            <SocialIcons />
+          </div>
+        </MDBNavbar>
     )
 }
 
