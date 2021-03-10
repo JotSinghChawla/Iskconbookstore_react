0768c208d00f4fa684d3d80b0f6cb3eb081c0eb0
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-11 00:10:52 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import HomePage from './HomePage'
+import Bookstore from './Bookstore';
+
+import { Switch, Route, Redirect } from 'react-router-dom'
+
 
 
 export class Main extends Component {
@@ -8,6 +13,31 @@ export class Main extends Component {
         return (
             <div>
                 <Header />
+                <Switch location={this.props.location}>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/bookstore' component={Bookstore} />
+                    <Route path='/aboutus' component={ () => {
+                        window.location.replace('https://www.iskcon.org/about-us/what-is-iskcon.php');
+                        return null;
+                    } } />
+                    <Route path='/contactus' component={ () => {
+                        window.location.replace('https://centres.iskcon.org');
+                        return null;
+                    } } />
+                    <Route path='/facebook' component={ () => {
+                        window.location.replace('https://www.facebook.com/iskconglobal');
+                        return null;
+                    } } />
+                    <Route path='/twitter' component={ () => {
+                        window.location.replace('https://twitter.com/india_iskcon');
+                        return null;
+                    } } />
+                    <Route path='/youtube' component={ () => {
+                        window.location.replace('https://www.youtube.com/channel/UCnQ0YFFLezzd5TEx4j8Dfzw');
+                        return null;
+                    } } />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
         )
