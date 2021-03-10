6d0ae7d63e19bb37a05856d1b3816efea25ca91a
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-11 00:09:33 +0530

    Small Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React,{ Component } from 'react';
 import Main from './components/Main';
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 import { HashRouter } from 'react-router-dom';
 
 import './App.css';
---
+++ src/components/Bookstore.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Bookstore extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Bookstore
