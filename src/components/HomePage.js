725d46c1ad4f9a8f2a8fb6e34d2a4c1a30232e7e
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-17 19:32:50 +0530

    Added Main Image in homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import { MDBAnimation, MDBIcon } from 'mdbreact'
+import styles from '../shared/homePage.module.css'
 
 export class HomePage extends Component {
     render() {
         return (
-            <div>
-                
+            <>
+            <div className={styles.mainImage}>
+                <div className={styles.imageText}>
+                        <h1 style={{ fontSize: '50px' }}> ISKCON - The Hare Krishna Movement </h1>
+                        <h3 className={ 'd-none d-sm-inline ' + styles.italicMain }>ananyas cintayanto mam ye janah paryupasate <br />
+                            tesam nityabhiyuktanam yoga-ksemam vahamy aham <br />
+                        </h3>
+                    <div style={{ marginTop: '60px'}}> 
+                        <MDBAnimation type="fadeInDown" duration="1.5s" infinite>
+                            <MDBIcon icon="angle-double-down" className="amber-text mt-3" size="3x" />
+                        </MDBAnimation>
+                        </div>
+                </div>
             </div>
+            <div>
+                <h1>HELLO WORLD</h1>
+            </div>
+            </>
         )
     }
 }
